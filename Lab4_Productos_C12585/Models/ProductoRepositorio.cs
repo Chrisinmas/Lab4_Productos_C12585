a1092baeba980331d0376d7c177e85333399fc7d
Christopher <duartebarahonachristopher1@gmail.com>
2026-04-17 18:37:30 -0600

    feat: agregar repositorio ProductoRepositorio con operaciones CRUD

---
+++ Lab4_Productos_C12585/Models/ProductoRepositorio.cs
@@ -0,0 +1,39 @@
+﻿// Models/ProductoRepositorio.cs
+namespace Lab4_Productos_C12585.Models
+{
+    public class ProductoRepositorio
+    {
+        private static List<Producto> _productos = new List<Producto>();
+        private static int _contadorId = 1;
+
+        public List<Producto> ObtenerTodos()
+        {
+            return _productos;
+        }
+
+        public Producto? ObtenerPorId(int id)
+        {
+            return _productos.FirstOrDefault(p => p.Id == id);
+        }
+
+        public void Agregar(Producto producto)
+        {
+            producto.Id = _contadorId++;
+            _productos.Add(producto);
+        }
+
+        public void Actualizar(Producto producto)
+        {
+            var indice = _productos.FindIndex(p => p.Id == producto.Id);
+            if (indice >= 0)
+                _productos[indice] = producto;
+        }
+
+        public void Eliminar(int id)
+        {
+            var producto = ObtenerPorId(id);
+            if (producto != null)
+                _productos.Remove(producto);
+        }
+    }
+}
